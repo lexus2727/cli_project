dea80e6d98cc395807e7d6152bb5d46588d7ffef
Candice Parker <lexus2424@gmail.com>
2019-07-08 23:55:40 +0000

    updating scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,19 @@
 
 
-class Scraper
+class CoolShoes::Scraper
   
 def get_page
  page = Nokogiri::HTML(open("https://store.nike.com/us/en_us/pw/mens-nikeid-lifestyle-shoes/1k9Z7puZoneZoi3"))
+ name = page.css(".grid-item-info").css(".product-name")
+  puts page[0]
+  binding.pry
+  
+ 
  end
 
-def get_names
-  names = page.css(".grid-item-info").css(".product-name").css("p").children.map { |name| name.text }.compact
-  binding.pry
-end
+#def get_names
+  
+#end
  
 
 #def get_prices
@@ -22,11 +26,11 @@ end
 #names = get_name
 #prices = get_price
 
-def get_shoes
-(0...prices.size).each do |index|
-  puts "--- index: #{index +1} ---"
-  puts "Name: #{names[index]} | price: #{prices[index]}"
-  end
-end
+#def get_shoes
+#(0...prices.size).each do |index|
+ # puts "--- index: #{index +1} ---"
+ # puts "Name: #{names[index]} | price: #{prices[index]}"
+  #end
+#end
 end
  
