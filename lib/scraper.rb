01a3305f1cba6e4005d97aea2d8df92ea3f0f038
Candice Parker <lexus2424@gmail.com>
2019-07-08 22:40:23 +0000

    Scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,31 +1,32 @@
-require 'open-uri'
-require 'nokogiri'
 
 
 class Scraper
   
 def get_page
- page = Nokogiri::HTML(open
-end
+ page = Nokogiri::HTML(open("https://store.nike.com/us/en_us/pw/mens-nikeid-lifestyle-shoes/1k9Z7puZoneZoi3"))
+ end
 
 def get_names
+  names = page.css(".grid-item-info").css(".product-name").css("p").children.map { |name| name.text }.compact
+  binding.pry
 end
  
 
-def get_prices
+#def get_prices
   
-end
+#end
 
 
 
-scraper = Scraper.new
-names = get_name
-prices = get_price
+#scraper = Scraper.new
+#names = get_name
+#prices = get_price
 
 def get_shoes
 (0...prices.size).each do |index|
   puts "--- index: #{index +1} ---"
   puts "Name: #{names[index]} | price: #{prices[index]}"
   end
+end
 end
  
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,41 +1,27 @@
-class CoolShoes::CLI
+class CoolShoes::Shoe
+  attr_accessor :name, :price, :url, :description
   
-  def call
-  CoolShoes::Scraper
-  list_shoes
-  menu
-  thanks
-  end
+  @@all = []
   
-  def list_shoes
-  puts "Men's Nike Shoes"
-  shoes = CoolShoes::Shoe.all
-  shoes.each.with_index(1) {|s, index| puts "#{index}. #{s.name}- #{s.price}"
-  end
+  def initialize(name, price, url, description)
+    @name = name
+    @price = price
+    @url = url
+    @description = description
+    save
+  end 
+ 
+ def self.all
+   @@all
+ end
   
-  def menu
-    input =nil
-  while input != "exit"
-    puts "Please enter the number for the shoes you would like more information on or type exit to end:"
-    input = gets.strip.downcase
-    
-    if input.to_i > 0
-    selected = CoolShoes::Shoes.find_by_index(input.to_i - 1)
-    puts "#{selected.name} - #{selected.price}"
-    puts "#{selected.description}"
-    puts "To see shoe available shoe sizes and photos, go to #{selected.url}."
-    end
-    elsif input.downcase == "list"
-    list_shoes
-    else
-    puts "Invalid entry. Please try again."
-    end
-    end 
-    end
+ def save
+  self.class.all << self
+ end
 
-    def thanks
-    puts "Please come again soon. Happy Shopping!"
-    end
-    end
-  
-  
+ def self.find_by_index(index)
+ @@all[index]
+ 
+ 
+  end
+  end
