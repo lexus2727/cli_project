5e7d95a620d3d7ed38546324496b2b70fe56685d
Candice Parker <lexus2424@gmail.com>
2019-07-16 03:09:23 +0000

    update

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,16 +1,16 @@
 class CliProject::CLI
   
   def call
-  CliProject::Scraper.get_page
-  list_shoes
-  menu
-  thanks
+    CliProject::Scraper.get_page
+    list_shoes
+    menu
+    thanks
   end
   
   def list_shoes
-  puts "Men's Nike Shoes"
-  shoes = CliProject::Shoe.all
-  shoes.each.with_index(1) {|s, index| puts "#{index}. #{s.name}- #{s.price}"}
+    puts "Men's Nike Shoes"
+    shoes = CliProject::Shoe.all
+    shoes.each.with_index(1) {|s, index| puts "#{index}. #{s.name}- #{s.price}"}
   end
   
   def menu
@@ -20,25 +20,25 @@ class CliProject::CLI
     input = gets.strip.downcase
     
     if input.to_i > 0
-    selected = CliProject::Shoe.find_by_index(input.to_i - 1)
-   # selected.add_description(input.to_i)
+      selected = CliProject::Shoe.find_by_index(input.to_i - 1)
+   
     
-     if selected == nil
-       puts "Invalid entry. Please try again."
+      if selected == nil
+        puts "Invalid entry. Please try again."
      else
        #selected is now an instance of a shoe, we'll pass that entire instance to our scraper
-      puts CliProject::Scraper.get_shoe_details(selected)
-       puts = " "
-    puts "#{selected.name} - #{selected.price}"
-     puts = "\n"
-    puts "#{selected.description}"
-    puts "To see shoe available, shoe sizes and photos, go to #{selected.url}."
+        puts CliProject::Scraper.get_shoe_details(selected)
+        puts = " "
+        puts "#{selected.name} - #{selected.price}"
+        puts = "\n"
+        puts "#{selected.description}"
+        puts "To see shoe available, shoe sizes and photos, go to #{selected.url}."
     
     #puts the details from the newly updated shoe instance
-    puts " "
+        puts " "
     end
-    elsif input.downcase == "list"
-    list_shoes
+      elsif input.downcase == "list"
+        list_shoes
     #else
     
     #puts "Invalid entry. Please try again."
